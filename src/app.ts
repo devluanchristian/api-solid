eed8cc1cf20f4e19c86d1a09ec688f60280ec696
Luan <luanchristian.rochadocarmo@gmail.com>
2023-07-18 10:44:52 -0300

    feat: implementação JWT

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import fastify from 'fastify'
 
 import { appRoutes } from './http/routes'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
 
 export const app = fastify()
+
+// Auth
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
