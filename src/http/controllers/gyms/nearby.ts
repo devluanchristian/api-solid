ab220f97e6d43ce3a4332c1d8caa4548167a7f7e
Luan <luanchristian.rochadocarmo@gmail.com>
2023-07-20 14:14:47 -0300

    feat: outros controllers da academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeFetchNearbyGymUseCase } from '@/use-cases/factories/make-fetch-nearby-gym-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  // FACTORY PATTERN
+  const nearbyGymUseCase = makeFetchNearbyGymUseCase()
+
+  const { gyms } = await nearbyGymUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
